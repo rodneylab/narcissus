83e8c3f5ac753cf1406ddfd46e5faf8175a76022
Rodney Lab <codesign@rodneylab.com>
2021-10-28 05:19:19 +0100

    fix: 💄 style comments

---
+++ src/lib/components/Comments.css.ts
@@ -0,0 +1,32 @@
+import { themeVars } from '$lib/styles/themes/theme.css';
+import { fontFamilyHeading, fontWeightSemibold } from '$lib/styles/vars/font.css';
+import { spacing0, spacing12, spacing2, spacingPx } from '$lib/styles/vars/spacing.css';
+import { style } from '@vanilla-extract/css';
+
+export const commentListItem = style({
+  listStyleType: 'none',
+});
+
+export const commentContainer = style({
+  marginBottom: [spacing12],
+});
+
+export const commentContent = style({
+  borderStyle: 'solid',
+  borderWidth: [spacingPx],
+  width: '70%',
+  borderColor: themeVars.colour.secondary,
+  boxShadow: `0.125rem 0.25rem 0.25rem ${themeVars.colour.shadow}`,
+  backgroundColor: themeVars.colour.accent,
+  paddingTop: spacing2,
+  color: themeVars.colour.alternative,
+});
+
+export const authorText = style({
+  margin: [spacing0],
+});
+
+export const dateText = style({
+  fontFamily: [fontFamilyHeading],
+  fontWeight: [fontWeightSemibold],
+});
--- src/lib/components/Comments.svelte
+++ src/lib/components/Comments.svelte
@@ -1,9 +1,16 @@
-<script>
+<script lang="ts">
+  import Card from '$lib/components/Card.svelte';
+  import {
+    authorText,
+    commentContainer,
+    commentContent,
+    commentListItem,
+    dateText,
+  } from '$lib/components/Comments.css';
   import dayjs from 'dayjs';
   import 'dayjs/locale/en-gb.js';
   import localizedFormat from 'dayjs/plugin/localizedFormat.js';
   import relativeTime from 'dayjs/plugin/relativeTime.js';
-  import Card from '$lib/components/Card.svelte';
 
   export let comments;
 
@@ -12,19 +19,21 @@
   dayjs.locale('en-gb');
 </script>
 
-<h2>Comments</h2>
-<ul>
-  {#each comments as { created_at: date, author, text }}
-    <li>
-      <Card>
-        <h3>{author}</h3>
-        <p>{text}</p>
-        <div>
-          <small>
-            {dayjs(date).fromNow()}
-          </small>
-        </div>
-      </Card>
-    </li>
-  {/each}
-</ul>
+<section>
+  <h2>Visitor Comments</h2>
+  <ul>
+    {#each comments as { created_at: date, author, text }}
+      <li class={commentListItem}>
+        <Card containerClass={commentContainer} contentClass={commentContent}>
+          <h3 class={authorText}>{author}</h3>
+          <p>{text}</p>
+          <div>
+            <small class={dateText}>
+              {dayjs(date).fromNow()}
+            </small>
+          </div>
+        </Card>
+      </li>
+    {/each}
+  </ul>
+</section>
