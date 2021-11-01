84d38240b85866635eb31ddf4d1c7cfead919a6e
Rodney Lab <codesign@rodneylab.com>
2021-11-01 13:37:43 +0000

    fix: 💫 update http headers

--- generate-headers.js
+++ generate-headers.js
@@ -1,25 +1,41 @@
 import Base64 from 'crypto-js/enc-base64.js';
 import sha256 from 'crypto-js/sha256.js';
 import { config } from 'dotenv';
-import fs, { promises as fsp } from 'fs';
+import fs from 'fs';
 import { parse } from 'node-html-parser';
 import path from 'path';
-import { fix_and_destroy_block } from 'svelte/internal';
 
 config();
 
 const __dirname = path.resolve();
 const rootDomain = process.env.VITE_DOMAIN;
 
-const findHtmlFiles = (startPath, filter = /\.html$/) => {
-  let startPathExists = false;
-  try {
-    fsp.access(startPath, fs.constants.F_OK);
-    startPathExists = true;
-  } catch {
+const buildDir = path.join(__dirname, 'build');
+const buildPathLength = path.dirname(buildDir).length + path.basename(buildDir).length + 1;
+
+let pageCspElements = [];
+function findHtmlFiles(startPath, filter = /\.html$/) {
+  if (!fs.existsSync(startPath)) {
     console.log(`Missing file: ${startPath}`);
+    return;
   }
-};
+  const files = fs.readdirSync(startPath);
+  files.forEach((element) => {
+    const filename = path.join(startPath, element);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      findHtmlFiles(filename, filter);
+    } else if (filter.test(filename)) {
+      const route = path.dirname(filename).slice(buildPathLength);
+      pageCspElements.push(`${route[0] === '/' ? route : `/${route}`}
+  Content-Security-Policy-Report-Only: base-uri 'self'; child-src 'self';  connect-src 'self' ws://${
+    process.env.VITE_DOMAIN
+  } https://hcaptcha.com https://*.hcaptcha.com ${
+        process.env.VITE_WORKER_URL
+      }; img-src 'self' data\:; font-src 'self' data\:; form-action 'self'; frame-ancestors 'self'; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com; manifest-src 'self'; media-src 'self' data\:; object-src 'none';`);
+    }
+  });
+}
 
 const htmlSourceFile = path.join(__dirname, 'build/index.html');
 const htmlSource = fs.readFileSync(htmlSourceFile, { encoding: 'utf-8' });
@@ -38,20 +54,39 @@ styleAttributesTags.forEach((tag) => {
 
 const styleSrcAttrCsp = [...styleSrcAttrHashes].map((element) => `'sha-256-${element}'`).join(' ');
 
-const headers = `/*
+function createHeaders() {
+  const headers = `/*
   X-Frame-Options: DENY
   X-XSS-Protection: 1; mode=block
   X-Content-Type-Options: nosniff
   Referrer-Policy: same-origin
   Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()
   Strict-Transport-Security: max-age=31536000; includeSubDomains; preload
-  #Content-Security-Policy-Report-Only: base-uri 'self'; child-src 'self'; connect-src 'self' ws://${process.env.VITE_DOMAIN} https://hcaptcha.com https://*.hcaptcha.com ${process.env.VITE_WORKER_URL}; img-src 'self' data:; font-src 'self' data:; form-action 'self'; frame-ancestors 'self'; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com; manifest-src 'self'; media-src 'self' data:; object-src 'none'; style-src 'self' 'unsafe-hashes' ${styleSrcAttrCsp} https://hcaptcha.com https://*.hcaptcha.com; default-src 'self' ${rootDomain} ws://${rootDomain}; script-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com; worker-src 'self'; report-to csp-endpoint; report-uri https://sentry.io/api/${process.env.SENTRY_PROJECT_ID}/security/?sentry_key=${process.env.SENTRY_KEY};
-  #Content-Security-Policy-Report-Only: base-uri 'self'; child-src 'self'; connect-src 'self' ws://${process.env.VITE_DOMAIN} https://hcaptcha.com https://*.hcaptcha.com ${process.env.VITE_WORKER_URL}; img-src 'self' data\:; font-src 'self' data\:; form-action 'self'; frame-ancestors 'self'; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com; manifest-src 'self'; media-src 'self' data\:; object-src 'none'; style-src 'self' 'unsafe-hashes' ${styleSrcAttrCsp} https://hcaptcha.com https://*.hcaptcha.com; default-src 'self' ${rootDomain} ws://${rootDomain}; script-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com; worker-src 'self'; report-to csp-endpoint; report-uri https://sentry.io/api/${process.env.SENTRY_PROJECT_ID}/security/?sentry_key=${process.env.SENTRY_KEY};
-  Content-Security-Policy-Report-Only: default-src 'none';
-  Report-To: {"group": "csp-endpoint", "max_age": 10886400, "endpoints": [{"url": "https://sentry.io/api/${process.env.SENTRY_PROJECT_ID}/security/?sentry_key=${process.env.SENTRY_KEY}"}]}
+  Report-To: {"group": "csp-endpoint", "max_age": 10886400, "endpoints": [{"url": "https://sentry.io/api/${
+    process.env.SENTRY_PROJECT_ID
+  }/security/?sentry_key=${process.env.SENTRY_KEY}"}]}
+${pageCspElements.join('\n')}
 `;
 
-const headersFile = path.join(__dirname, 'build/_headers');
+  const headersFile = path.join(buildDir, '_headers');
+  console.log('headers: ', headers);
+  fs.writeFileSync(headersFile, headers);
+}
 
-console.log(headers);
-fs.writeFileSync(headersFile, headers);
+async function main() {
+  findHtmlFiles(buildDir);
+  console.log(pageCspElements);
+  createHeaders();
+  console.log(pageCspElements);
+}
+
+main();
+
+// #Content-Security-Policy-Report-Only: base-uri 'self'; child-src 'self'; connect-src 'self' ws://${
+//     process.env.VITE_DOMAIN
+//   } https://hcaptcha.com https://*.hcaptcha.com ${
+//     process.env.VITE_WORKER_URL
+//   }; img-src 'self' data\:; font-src 'self' data\:; form-action 'self'; frame-ancestors 'self'; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com; manifest-src 'self'; media-src 'self' data\:; object-src 'none'; style-src 'self' 'unsafe-hashes' ${styleSrcAttrCsp} https://hcaptcha.com https://*.hcaptcha.com; default-src 'self' ${rootDomain} ws://${rootDomain}; script-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com; worker-src 'self'; report-to csp-endpoint; report-uri https://sentry.io/api/${
+//     process.env.SENTRY_PROJECT_ID
+//   }/security/?sentry_key=${process.env.SENTRY_KEY};
+//   Content-Security-Policy-Report-Only: default-src 'none';
