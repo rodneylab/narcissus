bd87a66bddf12d957ec726ada3f161a794b03106
Rodney Lab <codesign@rodneylab.com>
2021-11-01 13:58:27 +0000

    fix: 💄 update styles

--- src/lib/styles/styles.css.ts
+++ src/lib/styles/styles.css.ts
@@ -35,7 +35,7 @@ globalStyle('hr', {
   border: [spacing0],
 });
 
-globalStyle('html & :is(h1, h2, h3, h4, h5, h6, h7)', {
+globalStyle('html &:is(h1, h2, h3, h4, h5, h6, h7)', {
   margin: [spacing12, spacing0, spacing6],
   fontWeight: [fontWeightNormal],
 });
