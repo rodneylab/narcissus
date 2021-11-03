fdf4eded7432719ee6ed6ef89c6e6cb005f2b418
Rodney Lab <codesign@rodneylab.com>
2021-11-03 12:59:54 +0000

    ci: 🐝 update ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-  root: true,
-  extends: ['eslint:recommended', 'prettier'],
-  plugins: ['svelte3'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019,
-  },
   env: {
     browser: true,
-    es2017: true,
+    es2021: true,
     node: true,
   },
+  extends: ['eslint:recommended', 'prettier', 'airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  rules: {},
 };
