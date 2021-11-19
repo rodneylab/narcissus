0683a8268dce02fed34e41f673f848a1c1b78d62
Rodney Lab <codesign@rodneylab.com>
2021-11-19 05:05:57 +0000

    ci(narcissus/root): 🐝 update commitlint rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,25 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "apps/narcissus-dashboard",
+        "demos/narcissus-astro",
+        "demos/narcissus-sveltekit",
+        "narcissus/root",
+        "services/narcissus-api",
+        "apps",
+        "demos",
+        "narcissus",
+        "narcissus-api",
+        "narcissus-astro",
+        "narcissus-dashboard",
+        "narcissus-sveltekit",
+        "root",
+      ],
+    ],
+    "scope-empty": [2, "never"],
+  },
+};
