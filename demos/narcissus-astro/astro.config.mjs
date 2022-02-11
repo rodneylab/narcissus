378df4586d2c49abf266b339aa8c4ee319fa2337
Rodney Lab <codesign@rodneylab.com>
2022-02-11 05:25:40 +0000

    fix(demos/narcissus-astro): 💫 move aliases from astro.config to tsconfig

--- demos/narcissus-astro/astro.config.mjs
+++ demos/narcissus-astro/astro.config.mjs
@@ -1,6 +1,7 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import path from 'node:path';
 import { imagetools } from 'vite-imagetools';
+import urls from 'rehype-urls';
 
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
@@ -16,19 +17,29 @@ export default {
     // port: 3000,             // The port to run the dev server on.
     // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
+  markdownOptions: {
+    render: [
+      '@astrojs/markdown-remark',
+      {
+        rehypePlugins: [
+          [
+            'rehype-slug',
+            [
+              urls,
+              (url, node) => {
+                if (url.host !== 'internal.site') {
+                  node.properties.target = '_blank';
+                  node.properties.rel = 'nofollow noopener noreferrer';
+                }
+              },
+            ],
+          ],
+        ],
+      },
+    ],
+  },
   renderers: ['@astrojs/renderer-react', '@astrojs/renderer-svelte'],
   vite: {
-    resolve: {
-      alias: {
-        $components: path.resolve('./src/components'),
-        $configuration: path.resolve('./src/configuration'),
-        $constants: path.resolve('./src/constants'),
-        $hooks: path.resolve('./src/hooks'),
-        $layouts: path.resolve('./src/layouts'),
-        $pages: path.resolve('./src/pages'),
-        $styles: path.resolve('./src/styles'),
-      },
-    },
     plugins: [vanillaExtractPlugin(), imagetools({ force: true })],
   },
 };
